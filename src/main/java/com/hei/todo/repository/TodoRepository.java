93f9c0a866f0b6ef026c1beb50205436b8e25394
Miriasme <johndoe@example.com>
2026-07-26 22:11:23 +0300

    feat(Repository): add new query

--- src/main/java/com/hei/todo/repository/TodoRepository.java
+++ src/main/java/com/hei/todo/repository/TodoRepository.java
@@ -1,7 +1,14 @@
 package com.hei.todo.repository;
 
 import com.hei.todo.model.JTodo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoRepository extends JpaRepository<JTodo,String> {
+import java.util.List;
+import java.util.UUID;
+
+public interface TodoRepository extends JpaRepository<JTodo,UUID> {
+    Page<JTodo> findByCompleted(boolean completed, Pageable pageable);
+
 }
