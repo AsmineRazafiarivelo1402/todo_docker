8fb52ae12b2df55a4058105b4c034db855c2290f
Miriasme <johndoe@example.com>
2026-07-26 22:08:42 +0300

    feat(controller): add new endpoints

--- src/main/java/com/hei/todo/controller/TodoController.java
+++ src/main/java/com/hei/todo/controller/TodoController.java
@@ -2,14 +2,16 @@ package com.hei.todo.controller;
 
 import com.hei.todo.dto.TodoRequest;
 import com.hei.todo.dto.TodoResponse;
+import com.hei.todo.model.JTodo;
 import com.hei.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/todo")
@@ -21,4 +23,29 @@ public class TodoController {
         TodoResponse response = service.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+//    @GetMapping
+//    @ResponseBody
+//    public String hello() {
+//        return "World";
+//    }
+
+    @GetMapping
+    public Page<TodoResponse> getTasksByCompletionStatus(
+            @RequestParam boolean completed,
+            Pageable pageable) {
+        return service.getTasksByCompletionStatus(completed, pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TodoResponse> findById(@PathVariable UUID id) {
+        TodoResponse response = service.findById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping
+    public ResponseEntity<TodoResponse> update(@RequestBody TodoRequest request) {
+        TodoResponse response = service.update(request);
+        return ResponseEntity.ok(response);
+    }
+
 }
